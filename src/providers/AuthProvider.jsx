a017b8e5abb0af66295663908fd90b20502c1e23
rafiul1000 <rafiulislam5075@gmail.com>
2023-10-03 22:20:31 +0600

    onAuthStateChange and integrate login

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
 import { createContext } from "react";
 
-import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import app from "../firebase/firebase.config";
+import { useEffect } from "react";
 
 export const AuthContext = createContext(null);
 
@@ -16,10 +17,29 @@ const AuthProvider = ({children}) => {
     const createUser = (email, password) =>{
         return createUserWithEmailAndPassword(auth, email, password);
     }
+    const signIn = (email, password) => {
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+
+    const logOut = () => {
+        return signOut(auth)
+    }
+
+    useEffect(() => {
+        const unSubscribe =  onAuthStateChanged(auth, currentUser =>{
+            console.log('user in the authStateChanged', currentUser);
+            setUser(currentUser);
+        });
+        return () => {
+            unSubscribe();
+        }
+    }, [])
 
     const authInfo = {
         user,
-        createUser
+        createUser,
+        signIn,
+        logOut
     }
 
     return (
