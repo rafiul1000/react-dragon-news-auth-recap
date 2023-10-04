a8f95eaaf89057de60cb725e977239a5932e7a42
rafiul1000 <rafiulislam5075@gmail.com>
2023-10-04 16:25:04 +0600

    updated firebase config

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -5,7 +5,7 @@ import { Navigate, useLocation } from "react-router-dom";
 const PrivateRoute = ({children}) => {
     const {user, loading} = useContext(AuthContext);
     const location = useLocation();
-    console.log(location);
+    console.log(location.pathname);
 
     if(loading){
         return <span className="loading loading-infinity loading-lg"></span>
@@ -15,7 +15,7 @@ const PrivateRoute = ({children}) => {
         return children;
     }
 
-    return <Navigate to='/login'></Navigate>;
+    return <Navigate state={location.pathname} to='/login'></Navigate>;
 };
 
 export default PrivateRoute;
