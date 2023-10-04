1b03f2992ebe29a19fb5de4d91fe672d03ff22d0
rafiul1000 <rafiulislam5075@gmail.com>
2023-10-04 15:43:49 +0600

    Create Private route and do not redirect on reload

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -13,15 +13,19 @@ const auth = getAuth(app);
 const AuthProvider = ({children}) => {
 
     const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
 
     const createUser = (email, password) =>{
+        setLoading(true);
         return createUserWithEmailAndPassword(auth, email, password);
     }
     const signIn = (email, password) => {
+        setLoading(true)
         return signInWithEmailAndPassword(auth, email, password);
     }
 
     const logOut = () => {
+        setLoading(true)
         return signOut(auth)
     }
 
@@ -29,6 +33,7 @@ const AuthProvider = ({children}) => {
         const unSubscribe =  onAuthStateChanged(auth, currentUser =>{
             console.log('user in the authStateChanged', currentUser);
             setUser(currentUser);
+            setLoading(false);
         });
         return () => {
             unSubscribe();
@@ -37,6 +42,7 @@ const AuthProvider = ({children}) => {
 
     const authInfo = {
         user,
+        loading,
         createUser,
         signIn,
         logOut
